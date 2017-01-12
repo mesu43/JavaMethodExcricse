75ee077847135327c56e52e25a3837231a3dcfe7
Sumon Islam <islamsumon10@gmail.com>
2017-01-12 15:31:10 -0500

    add new method for excricise

--- src/method/practice/NewMethod.java
+++ src/method/practice/NewMethod.java
@@ -1,9 +1,11 @@
 package method.practice;
 
+import java.util.ArrayList;
+
 public class NewMethod {
 	
 	public static void main(String[] args) {
-		
+		System.out.println("Ans#2." + getAllEvenNumber(10));
 		System.out.println("Ans#1." + getName("this is String"));
 		
 	}
@@ -18,4 +20,20 @@ public class NewMethod {
 		 yourName = name;
 		 return yourName;
 	 }
+     /**Q.2
+	  * write a method that take a number input and return the list of all even
+	  * number from 0 to that given number.
+	  * parameter/input:int number
+	  * returnType/output:ArrayList<Integer>
+	  * servingBucket/container:List
+	  */
+	public static ArrayList<Integer> getAllEvenNumber(int number) {
+		ArrayList<Integer> numberList = new ArrayList<Integer>();
+		for (int i = 0; i <= number; i++) {
+			if (i % 2 == 0) {
+				numberList.add(i);
+			}
+		}
+		return numberList;
+	}
 }
